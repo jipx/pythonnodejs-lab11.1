63d513735d738a0c871fd9d345c38934c7eaa4b1
jipx <pengxiang00@gmail.com>
2023-10-05 15:56:59 +0800

    linking frontend with mock api gateway error

--- demo/resources/website/config.js
+++ demo/resources/website/config.js
@@ -1,4 +1,5 @@
 window.COFFEE_CONFIG = {
-	API_GW_BASE_URL_STR: "https://nr6xb0vno3.execute-api.us-east-1.amazonaws.com/prod",
-	COGNITO_LOGIN_BASE_URL_STR: null
+  //	API_GW_BASE_URL_STR: "https://ma98uvrspj.execute-api.us-east-1.amazonaws.com/prod",
+  API_GW_BASE_URL_STR: null,
+  COGNITO_LOGIN_BASE_URL_STR: null,
 };
---
+++ demo/update_config.js
@@ -0,0 +1,38 @@
+const AWS = require("aws-sdk");
+
+// Initialize the AWS SDK with your region
+AWS.config.update({ region: "us-east-1" });
+
+// Create an AWS S3 client
+const s3 = new AWS.S3();
+
+async function uploadFile() {
+  try {
+    // Specify your bucket name
+    const bucketName = "jipx-04-10-2023-s3bucket";
+
+    // Specify the local file path
+    const filename = "./resources/website/config.js";
+
+    // Specify the S3 key (object key) for the file
+    const s3Key = "config.js";
+
+    // Upload the file to S3 with the specified content type and cache control
+    await s3
+      .upload({
+        Bucket: bucketName,
+        Key: s3Key,
+        Body: require("fs").createReadStream(filename),
+        ContentType: "application/js",
+        CacheControl: "max-age=0",
+      })
+      .promise();
+
+    console.log("DONE");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+// Call the uploadFile function to upload the file to S3
+uploadFile();
