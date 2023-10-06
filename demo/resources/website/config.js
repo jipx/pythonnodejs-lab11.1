f0842f0ff945bd9474b07283d5d8921a742e737f
EC2 Default User <ec2-user@ip-172-31-61-230.ec2.internal>
2023-10-06 05:03:28 +0000

    working on cognito

--- demo/resources/website/config.js
+++ demo/resources/website/config.js
@@ -1,5 +1,5 @@
 window.COFFEE_CONFIG = {
   API_GW_BASE_URL_STR: "https://ma98uvrspj.execute-api.us-east-1.amazonaws.com/prod",
  // API_GW_BASE_URL_STR: null,
-  COGNITO_LOGIN_BASE_URL_STR: null,
+  COGNITO_LOGIN_BASE_URL_STR: "https://jipx-2023-10-41.auth.us-east-1.amazoncognito.com/login?client_id=iq7vs8lgsufg0tm98jrf65vm2&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fjipx-04-10-2023-s3bucket.s3.amazonaws.com%2Fcallback.html",
 };
