6aaee9d0ef9da1eef9d7f67069926897ddc11df5
EC2 Default User <ec2-user@ip-172-31-61-230.ec2.internal>
2023-10-06 02:48:22 +0000

    api gateway linked with front end, except report

--- demo/resources/website/config.js
+++ demo/resources/website/config.js
@@ -1,5 +1,5 @@
 window.COFFEE_CONFIG = {
-  //	API_GW_BASE_URL_STR: "https://ma98uvrspj.execute-api.us-east-1.amazonaws.com/prod",
-  API_GW_BASE_URL_STR: null,
+  API_GW_BASE_URL_STR: "https://ma98uvrspj.execute-api.us-east-1.amazonaws.com/prod",
+ // API_GW_BASE_URL_STR: null,
   COGNITO_LOGIN_BASE_URL_STR: null,
 };
